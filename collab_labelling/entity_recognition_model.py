5f98399f1962beba9c74e85e8352beb43bcf1358
Cheryl <cheryl.nqj@gmail.com>
2021-10-29 21:35:26 +0800

    Allow entity recognition to run on multiple columns

--- collab_labelling/entity_recognition_model.py
+++ collab_labelling/entity_recognition_model.py
@@ -5,6 +5,7 @@ from google.cloud import language
 
 ### COMMANDS TO RUN THIS:
 ### pip install -r requirements.txt
+### if using spacy model, run: "python -m spacy download en_core_web_lg"
 ### python entity_recognition_model.py (ensure input file path is valid)
 
 from enum import Enum
@@ -12,7 +13,7 @@ class Model(Enum):
     GOOGLE_MODEL = "google_nlp"
     SPACY_MODEL = "spacy"
 
-ENTITY_RECOGNITION_MODEL = "google_nlp"     # options: google_nlp, spacy
+ENTITY_RECOGNITION_MODEL = Model.GOOGLE_MODEL     # options: google_nlp, spacy
 
 if (ENTITY_RECOGNITION_MODEL == Model.GOOGLE_MODEL):
     # ensure that containing folder contains a file named gservice_account.json, storing the service account key info. Follow https://cloud.google.com/natural-language/docs/quickstart-client-libraries
@@ -25,7 +26,7 @@ elif (ENTITY_RECOGNITION_MODEL == Model.SPACY_MODEL):
 FILE_TYPE = ".csv"
 INPUT_CSV_FILE_NAME = "test_videos"
 INPUT_CSV_FILE_PATH = INPUT_CSV_FILE_NAME + FILE_TYPE
-OUTPUT_CSV_FILE_PATH = INPUT_CSV_FILE_NAME + "_entities_spacy" + FILE_TYPE
+OUTPUT_CSV_FILE_PATH = INPUT_CSV_FILE_NAME + "_entities_new" + FILE_TYPE
 
 print("Generating entity results at {}".format(OUTPUT_CSV_FILE_PATH))
 
@@ -57,12 +58,18 @@ def analyze_text_entities_for_video(video_id, text):
     print("Entities found and added for video ID {}".format(video_id))
 
 def run_entity_recogniser_on_csv():
-    SELECTED_COL_NAME = "Title"
+    SELECTED_COLS = ["Title", "Description"]
     VIDEO_ID_COL_NAME = "VideoID"
     
+    print("Selected Columns to Run ER Model on: {}".format(",".join(SELECTED_COLS)))
+
     df = pd.read_csv(INPUT_CSV_FILE_PATH)
     for index, row in df.iterrows():
-        selected_text = df.loc[index, SELECTED_COL_NAME]
+        selected_text = ""
+        for column in SELECTED_COLS:
+            new_text = df.loc[index, column]
+            if type(new_text) == str:
+                selected_text += " " + new_text
         selected_video_id = df.loc[index, VIDEO_ID_COL_NAME]
         try:
             analyze_text_entities_for_video(selected_video_id, selected_text)
